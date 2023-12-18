e0af5c6c53447e5dfbb82e8df617a0a065de6859
IMAN <Imannursoleh610@gmail.com>
2023-12-18 22:17:59 +0700

    Initial commit by iman

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import os
 from pymongo import MongoClient
+from flask import request
 from os.path import join, dirname
 from dotenv import load_dotenv
 import time
+from bson import ObjectId
 import jwt
 from datetime import datetime, timedelta
 import hashlib
@@ -52,7 +54,14 @@ def login_admin():
 
 @app.route('/Gallery_nus')
 def Gallery_nus():
-    return render_template('Gallery.html')
+    msg = request.args.get('msg')
+    return render_template('Gallery.html',  msg=msg)
+
+
+@app.route('/booking/<title>')
+def booking_title(title):
+    title_info = db.data_wisata.find_one({'title': title})    
+    return render_template('booking.html', title_info=title_info)
 
 # @app.route("/admin_reg")
 # def admin_register():
@@ -245,6 +254,19 @@ def gallery():
         return render_template('Gallery.html', user_info=user_info)        
     except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):        
         return redirect(url_for('home'))
+    
+@app.route('/gallery_list', methods=['GET'])
+def gallery_list():
+    try:
+        # Fetch data from the 'data_wisata' collection
+        articles = list(db.data_wisata.find({}, {'_id': False}))
+
+        # Return the data as JSON        
+        return jsonify({'articles': articles }), 200
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+        return jsonify({'error': 'Failed to fetch data'}), 500
+
 
 
 @app.route('/booking')
@@ -291,7 +313,8 @@ def data_view():
         return render_template('Data_View.html', user_info=user_info)        
     except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):        
         return redirect(url_for('home'))
-  
+
+
 
 @app.route('/data_wisata')
 def data_wisata():
@@ -307,6 +330,24 @@ def data_wisata():
     except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):        
         return redirect(url_for('data_view'))
 
+
+# @app.route('/find_wisata/<title>', methods=['GET'])
+# def find_wisata(title):
+#     try:
+#         # Query the database for a document with the given title
+#         wisata = db.data_wisata.find_one({'title': title})
+
+#         # If a document with the given title was found, return it
+#         if wisata is not None:
+#             return jsonify(wisata), 200
+
+#         # If no document was found, return an error message
+#         return jsonify({'error': 'Wisata not found'}), 404
+
+#     except Exception as e:
+#         # If an error occurred, return an error message
+#         return jsonify({'error': 'An error occurred: ' + str(e)}), 500
+
 @app.route('/data_wisata_list', methods=['GET'])
 def data_wisata_list():
     try:
@@ -319,6 +360,71 @@ def data_wisata_list():
         print(f"An error occurred: {str(e)}")
         return jsonify({'error': 'Failed to fetch data'}), 500
 
+# @app.route('/data_wisata_list', methods=['GET'])
+# def data_wisata_list():
+#     try:
+#         # Fetch data from the 'data_wisata' collection
+#         articles = list(db.data_wisata.find({}, {'_id': True}))
+
+#         # Return the data as JSON
+#         return jsonify({'articles': articles }), 200
+#     except Exception as e:
+#         print(f"An error occurred: {str(e)}")
+#         return jsonify({'error': 'Failed to fetch data'}), 500
+
+
+#delete card
+# @app.route('/delete_article', methods=['POST'])
+# def delete_article():
+#     try:
+#         id = request.form['id']
+#         db.data_wisata.delete_one({'_id': ObjectId(id)})
+#         return jsonify({'msg': 'Deleted successfully'}), 200
+#     except Exception as e:
+#         print(f"An error occurred: {str(e)}")
+#         return jsonify({'error': 'Failed to delete article'}), 500
+
+# @app.route('/delete_article', methods=['POST'])
+# def delete_article():
+#     try:
+#         id = request.form['id']
+#         # Bagian kode lainnya
+#         return jsonify({'msg': 'Berhasil dihapus'}), 200
+#     except Exception as e:
+#         print(f"Terjadi kesalahan: {str(e)}")
+#         return jsonify({'error': 'Gagal menghapus artikel'}), 500
+
+# delete card
+# @app.route('/delete_article', methods=['POST'])
+# def delete_article():
+#     try:
+#         id = request.form['id']
+#         db.data_wisata.delete_one({'_id': ObjectId(id)})
+#         return jsonify({'msg': 'Berhasil dihapus'}), 200
+#     except Exception as e:
+#         print(f"Terjadi kesalahan: {str(e)}")
+#         return jsonify({'error': 'Gagal menghapus artikel', 'detail': str(e)}), 500
+
+@app.route('/delete_card', methods=['POST'])
+def delete_card():
+    try:
+        title_receive = request.form['title_give']
+        db.data_wisata.delete_one({'title': title_receive})
+        return jsonify({'msg': f'Data dengan judul {title_receive} berhasil dihapus'}), 200
+    except Exception as e:
+        print(f"Terjadi kesalahan: {str(e)}")
+        return jsonify({'error': 'Gagal menghapus data', 'detail': str(e)}), 500
+
+# @app.route('/update_card', methods=['POST'])
+# def update_card():
+#     try:
+#         title_receive = request.form['title_give']
+#         new_title_receive = request.form['new_title_give']
+#         db.data_wisata.update_one({'title': title_receive}, {'$set': {'title': new_title_receive}})
+#         return jsonify({'msg': f'Data dengan judul {title_receive} berhasil diupdate menjadi {new_title_receive}'}), 200
+#     except Exception as e:
+#         print(f"Terjadi kesalahan: {str(e)}")
+#         return jsonify({'error': 'Gagal mengupdate data', 'detail': str(e)}), 500
 
 # @app.route('/wisata_add')
 # def wisata_add():
@@ -395,19 +501,180 @@ def wisata_add_form():
         print(f"Terjadi kesalahan: {str(e)}")
         return jsonify({'msg': 'Failed'}), 500
 
-@app.route('/wisata_edit')
-def wisata_edit():
-    token_receive = request.cookies.get(TOKEN_KEY)
-    try:
-        payload = jwt.decode(
-                token_receive,
-                SECRET_KEY,
-                algorithms=['HS256']
-            )
-        user_info = db.users.find_one({'username': payload.get('id')})
-        return render_template('Data_edit.html', user_info=user_info)        
-    except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):        
-        return redirect(url_for('data_view'))
+
+# @app.route('/wisata_edit/<title>', methods=['GET'])
+# def wisata_edit(title):
+#     token_receive = request.cookies.get(TOKEN_KEY)
+#     try:
+#         payload = jwt.decode(
+#                 token_receive,
+#                 SECRET_KEY,
+#                 algorithms=['HS256']
+#             )
+#         user_info = db.users.find_one({'username': payload.get('id')})
+#         wisata_info = db.wisata.find_one({'title': title})  # Sesuaikan dengan parameter title
+#         return render_template('Data_edit.html', user_info=user_info, wisata_info=wisata_info)
+#     except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):        
+#         return redirect(url_for('data_view'))
+
+# def is_valid_title(title):
+#     MAX_TITLE_LENGTH = 100  # Define your own limit
+#     if title and len(title) <= MAX_TITLE_LENGTH:
+#         return True
+#     return False
+
+# @app.route('/wisata_update/<title>', methods=['POST'])
+# def wisata_(title):
+#     try:
+#         # Retrieve form data
+#         # Jika Anda mengganti id_receive menjadi title_receive atau sesuai dengan perubahan nama
+#         title_receive = request.form['title_give'] 
+#         if not is_valid_title(title_receive):
+#             return jsonify({'msg': 'Invalid title'}), 400
+
+#         # ... (lanjutkan dengan proses lainnya)
+#         file = request.files["file_give"]
+#         extension = file.filename.split('.')[-1]
+#         timestamp = int(time.time())  # Get current time in seconds since the Epoch
+#         filename = f'static/post_{timestamp}.{extension}'  # Append the timestamp to the filename
+#         file.save(filename)
+
+#         description_receive = request.form['description_give']
+
+#         paket1_receive = request.form['paket1_give']
+#         konten1_receive = request.form['description1_give']
+#         tiket1_receive = request.form['tiket1_give']
+#         harga1_receive = request.form['harga1_give']
+
+#         paket2_receive = request.form['paket2_give']
+#         konten2_receive = request.form['description2_give']
+#         tiket2_receive = request.form['tiket2_give']
+#         harga2_receive = request.form['harga2_give']
+        
+#         new_values = {
+#             "$set": {
+#                 'file': filename,
+#                 'description': description_receive,
+#                 'paket1': {
+#                     'paket': paket1_receive,
+#                     'konten': konten1_receive,
+#                     'tiket': tiket1_receive,
+#                     'harga': harga1_receive,
+#                 },
+#                 'paket2': {
+#                     'paket': paket2_receive,
+#                     'konten': konten2_receive,
+#                     'tiket': tiket2_receive,
+#                     'harga': harga2_receive,
+#                 }
+#             }
+#         } 
+#         db.data_wisata.update_one({'title': title}, new_values)  # Sesuaikan dengan perubahan nama
+#         return jsonify({'msg': 'sukses'}), 200
+#     except Exception as e:
+#         print(f"Terjadi kesalahan: {str(e)}")
+#         return jsonify({'msg': 'Failed'}), 500
+
+
+# @app.route('/wisata_edit_form', methods=['POST'])
+# def wisata_edit_form():
+#     try:
+#         # Retrieve form data
+#         file = request.files["file_give"]
+#         extension = file.filename.split('.')[-1]
+#         timestamp = int(time.time())  # Get current time in seconds since the Epoch
+#         filename = f'static/post_{timestamp}.{extension}'  # Append the timestamp to the filename
+#         file.save(filename)
+
+#         title_receive = request.form['title_give']
+#         description_receive = request.form['description_give']
+
+#         paket1_receive = request.form['paket1_give']
+#         konten1_receive = request.form['description1_give']
+#         tiket1_receive = request.form['tiket1_give']
+#         harga1_receive = request.form['harga1_give']
+
+#         paket2_receive = request.form['paket2_give']
+#         konten2_receive = request.form['description2_give']
+#         tiket2_receive = request.form['tiket2_give']
+#         harga2_receive = request.form['harga2_give']
+
+#         # Process the form data
+#         new_values = {
+#             "$set": {
+#                 'file': filename,
+#                 'description': description_receive,
+#                 'paket1': {
+#                     'paket': paket1_receive,
+#                     'konten': konten1_receive,
+#                     'tiket': tiket1_receive,
+#                     'harga': harga1_receive,
+#                 },
+#                 'paket2': {
+#                     'paket': paket2_receive,
+#                     'konten': konten2_receive,
+#                     'tiket': tiket2_receive,
+#                     'harga': harga2_receive,
+#                 }
+#             }
+#         }
+
+#         db.data_wisata.update_one({'title': title_receive}, new_values)
+#         return jsonify({'msg': 'sukses'}), 200
+#     except Exception as e:
+#         print(f"Terjadi kesalahan: {str(e)}")
+#         return jsonify({'msg': 'Failed'}), 500
+
+# @app.route('/wisata_edit_form', methods=['POST'])
+# def wisata_edit_form():
+#     try:
+#         # Retrieve form data
+#         id_receive = request.form['id_give'] 
+#         file = request.files["file_give"]
+#         extension = file.filename.split('.')[-1]
+#         timestamp = int(time.time())  # Get current time in seconds since the Epoch
+#         filename = f'static/post_{timestamp}.{extension}'  # Append the timestamp to the filename
+#         file.save(filename)
+
+#         id_receive = request.form['id_give']  # Changed from title to id
+#         description_receive = request.form['description_give']
+
+#         paket1_receive = request.form['paket1_give']
+#         konten1_receive = request.form['description1_give']
+#         tiket1_receive = request.form['tiket1_give']
+#         harga1_receive = request.form['harga1_give']
+
+#         paket2_receive = request.form['paket2_give']
+#         konten2_receive = request.form['description2_give']
+#         tiket2_receive = request.form['tiket2_give']
+#         harga2_receive = request.form['harga2_give']
+
+#         # Process the form data
+#         new_values = {
+#             "$set": {
+#                 'file': filename,
+#                 'description': description_receive,
+#                 'paket1': {
+#                     'paket': paket1_receive,
+#                     'konten': konten1_receive,
+#                     'tiket': tiket1_receive,
+#                     'harga': harga1_receive,
+#                 },
+#                 'paket2': {
+#                     'paket': paket2_receive,
+#                     'konten': konten2_receive,
+#                     'tiket': tiket2_receive,
+#                     'harga': harga2_receive,
+#                 }
+#             }
+#         } 
+#         db.data_wisata.update_one({'_id': ObjectId(id_receive)}, new_values)  # Changed from title to id
+#         return jsonify({'msg': 'sukses'}), 200
+#     except Exception as e:
+#         print(f"Terjadi kesalahan: {str(e)}")
+#         return jsonify({'msg': 'Failed'}), 500
+#     if not is_valid_object_id(id_receive):
+#         return jsonify({'msg': 'Invalid id'}), 400
  
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
